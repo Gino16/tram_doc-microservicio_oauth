d584af4ccd1cd12d1cdf4d171572b87b13e2abb1
Gino Ascencio <gino.francisco@hotmail.com>
2021-05-19 19:15:31 -0500

    Update PersonaFeignClient.java

--- src/main/java/com/tramite_documentario/microservicios/backend/microserviciooauth/clients/PersonaFeignClient.java
+++ src/main/java/com/tramite_documentario/microservicios/backend/microserviciooauth/clients/PersonaFeignClient.java
@@ -2,7 +2,6 @@ package com.tramite_documentario.microservicios.backend.microserviciooauth.clien
 
 import com.tramite_documentario.microservicio.backend.commonpersonas.models.entity.Persona;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
